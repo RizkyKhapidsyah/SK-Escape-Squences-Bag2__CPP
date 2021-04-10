963c9eb22b60b2ee11f401845d4c6e9083c3b87a
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-04-10 21:50:54 +0700

    Add project files.

---
+++ SK-Escape-Squences-Bag2__CPP/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+
+	cout << "Nama Saya \'Rizky Khapidsyah\' asal saya blablabla...\n";
+	cout << "Nama Saya \?Rizky Khapidsyah\? asal saya blablabla...\n";
+	cout << "Nama Saya \\Rizky Khapidsyah\\ asal saya blablabla...\n";
+	cout << "Nama Saya \aRizky Khapidsyah\a asal saya blablabla...\n";
+	cout << "Nama Saya \bRizky Khapidsyah\b asal saya blablabla...\n";
+
+	_getch();
+	return 0;
+}
